e0466cb453889e7d1d17d231ff1677e8323f5e5e
Grzegorz Szermiński <szerman@poczta.fm>
2018-03-20 21:37:46 +0100

    Add styles - WIP

--- components/Counter.js
+++ components/Counter.js
@@ -17,13 +17,32 @@ var Counter = React.createClass({
         });
     },
 
+    componentWillMount: function() {
+    console.log('Przed utworzeniem komponentu');
+    },
+
+    componentDidMount: function() {
+    console.log('Po utworzeniu komponentu');
+    },
+
+    componentWillUpdate: function(nextProps, nextState) {
+    
+    console.log('Aktualny stan licznika: ' + nextState.counter);
+
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+   
+    console.log('Poprzednia stan licznika: ' + prevState.counter);
+    },
+
     render: function() {
         return React.createElement('div', {},
             React.createElement('button', {onClick: this.decrement}, '-'),
             React.createElement('span', {}, 'Licznik ' + this.state.counter),
             React.createElement('button', {onClick: this.increment}, '+')
         );
-    }
+    }    
 });
 
 var element = React.createElement(Counter);
